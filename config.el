853c7a25f0b543912e539d2672a8ee42c120b0a4
Aqeel Akber <aqeel.akber@gmail.com>
2019-02-10 15:22:58 +1100

    Fixed quoting and added better prefixes

--- config.el
+++ config.el
@@ -14,9 +14,7 @@
 (setq org-octopress-directory-top "~/Blog/src")
 
 ;;; Journal
-(setq 'org-journal-dir "~/Journal/")
-(setq 'org-journal-date-format "%Y-%m-%d [%Y-%W-%u %j]")
-(setq 'org-journal-time-format "%H:%M:%S+%z")
-(setq 'org-journal-date-prefix "%Y-%m-%d [%Y-%W-%u %j]")
-(setq 'org-journal-time-prefix "%H:%M:%S+%z")
-(setq 'org-journal-enable-agenda-integration t)
+(setq org-journal-dir "~/Journal/")
+(setq org-journal-date-format "Date: %Y-%m-%d [%Y-%W-%u %j]")
+(setq org-journal-time-format "Entry: %H:%M:%S%z")
+(setq org-journal-enable-agenda-integration t)
